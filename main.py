b5fd938eaf8d087621b099ad79d1bb5302b48c60
David Prieto <david.s.prieto1@gmail.com>
2025-01-14 08:30:19 -0600

    added more endpoints to API

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/")
@@ -9,11 +18,27 @@ def root():
 
 
 @app.get("/posts/{post_id}")
-def get_posts(post_id: int):
+def get_post(post_id: int):
     return {f"data": f"This is your post with id: {post_id}"}
 
 
-@app.post("/post/create")
-def create_post(body: dict = Body(...)):
-    print(body)
-    return {"message": "Post successfully created"}
+@app.get("/posts")
+def get_all_posts():
+    return {f"data": f"These are all of the posts: "}
+
+
+@app.put("/posts/{post_id}")
+def update_post(post_id: int):
+    return {f"data": f"Successfully updated post with id: {post_id}"}
+
+
+@app.delete("/posts/{post_id}")
+def delete_post(post_id: int):
+    return {f"data": f"Successfully deleted post with id: {post_id}"}
+
+
+@app.post("/posts")
+def create_post(post: Post):
+    print(post)
+    new_post_to_dict = post.model_dump()
+    return {"data": new_post_to_dict}
