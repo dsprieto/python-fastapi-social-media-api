b6c51e8357b4825be7c5939922a8fa09ed19128a
David Prieto <david.s.prieto1@gmail.com>
2025-01-14 09:26:14 -0600

    added array for temporary storage of posts along with auto incrementing id - will obviously connect to database later

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List, Dict
 
 app = FastAPI()
+
+
+post_id: int = 1
 
 
 class Post(BaseModel):
@@ -12,33 +15,48 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+my_posts = []
+
+
+def search_by_id(posts_array: List[Dict], target_id: int):
+    """Search for a dictionary by 'id' field in a list of dictionaries."""
+    for post in posts_array:
+        if post['id'] == target_id:
+            return post
+    return None
+
+
 @app.get("/")
 def root():
     return {"message": "Welcome to my API"}
 
 
-@app.get("/posts/{post_id}")
-def get_post(post_id: int):
-    return {f"data": f"This is your post with id: {post_id}"}
+@app.post("/posts")
+def create_post(post: Post):
+    global post_id
+    post_to_dict = post.model_dump()
+    post_to_dict = {"id": post_id, **post_to_dict}
+    my_posts.append(post_to_dict)
+    post_id += 1
+    return {"data": f"successfully added post: {post_to_dict}"}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post: dict = search_by_id(my_posts, id)
+    return {f"data": post}
 
 
 @app.get("/posts")
 def get_all_posts():
-    return {f"data": f"These are all of the posts: "}
+    return {f"data": f"{my_posts}"}
 
 
-@app.put("/posts/{post_id}")
-def update_post(post_id: int):
-    return {f"data": f"Successfully updated post with id: {post_id}"}
+@app.put("/posts/{id}")
+def update_post(id: int):
+    return {f"data": f"Successfully updated post with id: {id}"}
 
 
-@app.delete("/posts/{post_id}")
-def delete_post(post_id: int):
-    return {f"data": f"Successfully deleted post with id: {post_id}"}
-
-
-@app.post("/posts")
-def create_post(post: Post):
-    print(post)
-    new_post_to_dict = post.model_dump()
-    return {"data": new_post_to_dict}
+@app.delete("/posts/{id}")
+def delete_post(id: int):
+    return {f"data": f"Successfully deleted post with id: {id}"}
